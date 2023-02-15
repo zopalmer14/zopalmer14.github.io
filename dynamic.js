7f8b64296aef9ff996489fb1688d3f5227ee039e
zopalmer14 <zopalmer14@gmail.com>
2023-02-15 14:54:00 -0500

    feat: copied over relevant files

---
+++ dynamic.js
@@ -0,0 +1,22 @@
+
+// JS file to allow dynamic animations / processes
+
+// function setup() {
+//   /* check for the window position relative to the header img whenever the user scrolls */  
+//   window.onscroll = function() {
+//     scrollFunction();
+//   }
+// }
+
+// function scrollFunction() {
+//   // check the coordinates of the background to see if it has exited the viewport
+//   var background = document.getElementById("hoh-background")
+//   if (background != null) {
+//     var background_pos = background.getBoundingClientRect();
+//     if (background_pos.bottom <= 0) {
+//       document.getElementById("magic-navbar").style.top = "0";
+//     } else {
+//       document.getElementById("magic-navbar").style.top = "-200px";
+//     }
+//   }
+// }
